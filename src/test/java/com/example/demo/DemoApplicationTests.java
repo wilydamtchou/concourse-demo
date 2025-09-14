733ee362f01ebc8d505515853bfe7c3cf9549500
Willy Damtchou <willydamtchou@2e:33:bb:29:b3:31.home>
2025-09-14 16:18:27 -0400

    dev

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void helloEndpointShouldReturnHelloWorld() {
+        String url = "http://localhost:" + port + "/";
+        String response = restTemplate.getForObject(url, String.class);
+
+        assertThat(response).isEqualTo("Hello World");
+    }
 }
